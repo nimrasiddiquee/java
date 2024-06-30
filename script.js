b5dd3820e9ef8de8eb191024ec92eba8f482934e
nimrasiddiquee <148622732+nimrasiddiquee@users.noreply.github.com>
2024-06-30 04:04:30 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// var num1=+ prompt("enter your name")
+// var num2=+ prompt("enter your first number")
+// var num3=+ prompt("enter your second number")
+// alert(num1+num2+num3)
+// alert(num1-num2-num3)
+//  var num = prompt("enter your number")
+// alert(num +"X"+ num +"=" + num *1)
+//      (num +"X"+ num +"=" + num *2)
+//      (num +"X"+ num +"=" + num *3)
+//      (num +"X"+ num +"=" + num *4)
+var email = prompt("enter your email")
+var password =prompt("enter your password")
+if (email == "kashan@gmail.com") {
+
+    alert("you are kashan")
+}
+else if(password == "kashan"){
+    alert("you are admin")
+}
+else
+{
+ alert("you are a user")}
